f708f941f81266313c0f7042cd9570d2aa6d2aaf
Snjezana Jaksic <snjezana.jaksic.sj@gmail.com>
2023-01-19 12:13:16 -0500

    POST/login updates

--- api/users.js
+++ api/users.js
@@ -16,4 +16,34 @@ usersRouter.get('/', async (req, res) => {
   });
 });
 
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 module.exports = usersRouter;
