bd7b2b9a8e1fec59fcf9a83b9705e70ff6f80186
Snjezana Jaksic <snjezana.jaksic.sj@gmail.com>
2023-01-19 11:29:43 -0500

    postRoute added

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const usersRouter = require('./users');
+const postsRouter = require('./posts')
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/posts', postsRouter);
 
 module.exports = apiRouter;
